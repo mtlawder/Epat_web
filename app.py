7341756c1a299a91ce615a21a89751c359b62c54
mtlawder <mtlawder@gmail.com>
2016-02-09 10:33:23 -0500

    full app

--- app.py
+++ app.py
@@ -7,23 +7,20 @@ from bokeh.document import Document
 
 app=Flask(__name__)
 
-#import urllib2
-#import json
-#import pandas as pd
-#import numpy as np
-#import os
-#import sqlite3
-#import datetime
 
-#import re
-#from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
+import sqlite3
+import datetime
+import re
+from sklearn.linear_model import LinearRegression
 
 #NODE_info=pd.read_csv('N_info.csv')
 #NODE_front=[NODE_info['NODE_NAME'][x].split(".")[0] for x in range(len(NODE_info))]
 
 @app.route('/')
 def main():
-    render_template('Onenode_plot.html')
+
     return redirect('/index_Main')
 
 @app.route('/index_Main',methods=['GET','POST'])
