ff762754baf2ee8f9160f33f99c1bce8b8482745
mtlawder <mtlawder@gmail.com>
2016-02-09 09:56:51 -0500

    both heroku and git

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
-@app.route('/index')
+@app.route('/index_Main')
 def index():
-  return render_template('index.html')
+    return render_template('Mileston_Main.html')
 
 if __name__ == '__main__':
-  app.run(host='159.203.65.80',port=33507)
+    app.run(host='159.203.65.80',port=33507)
